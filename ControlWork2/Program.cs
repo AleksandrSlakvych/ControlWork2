07c2a5d100bf0f08ce6e435d0db0ed8d80ddabc2
Александр Слаквич <aleksandr.mxr@gmail.com>
2019-06-02 16:05:10 +0300

    initial commit

--- ControlWork2/Program.cs
+++ ControlWork2/Program.cs
@@ -6,16 +6,131 @@ using System.Threading.Tasks;
 
 namespace ControlWork2
 {
+    abstract class File
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string Size { get; set; }
+
+        public File() { }
+
+        public override string ToString()
+        {
+            string result = "";
+            result += $"\t{Name}.{Extension}\n\t\tExtention: {Extension}\n\t\tSize: {Size}\n\t\t";
+            return result;
+        }
+        public abstract void Pars(string newInfo);
+    }
+
+    class TextFile : File
+    {
+        public string Content { get; set; }
+
+        public TextFile() { }
+
+        public override void Pars(string result)
+        {
+            string[] fileInfo = result.Split(new char[] { ':', '.', ';', '(', ')' });
+            Name = fileInfo[1];
+            Extension = fileInfo[2];
+            Size = fileInfo[3];
+            Content = fileInfo[5];
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            result += base.ToString() + $"Content: {Content}";
+            return result;
+        }
+    }
+
+    class MovieFile : File
+    {
+        public string Lenght { get; set; }
+        public string Resolution { get; set; }
+
+
+        public MovieFile() { }
+
+        public override void Pars(string result)
+        {
+            string[] fileInfo = result.Split(new char[] { ':', '.', ';', '(', ')' });
+            Name = fileInfo[1];
+            Name += fileInfo[2];
+            Extension = fileInfo[3];
+            Size = fileInfo[4];
+            Resolution = fileInfo[6];
+            Lenght = fileInfo[7];
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Resolution: {Resolution}\n\t\tLenght: {Lenght}";
+        }
+    }
+
+    class ImageFile : File
+    {
+        public string Resolution { get; set; }
+
+        public ImageFile() { }
+
+        public override void Pars(string result)
+        {
+            string[] fileInfo = result.Split(new char[] { ':', '.', ';', '(', ')' });
+            Name = fileInfo[1];
+            Extension = fileInfo[2];
+            Size = fileInfo[3];
+            Resolution = fileInfo[5];
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Resolution: {Resolution}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string s = @"Text:file.txt(6B);Some string content
+Image:img.bmp(19MB);1920x1080
+Text:data.txt(12B);Another string
+Text:data1.txt(7B);Yet another string
+Movie:logan.2017.mkv(19GB);1920x1080;2h12m";
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            var strToFile = s.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            List<File> files = new List<File>();
+
+            foreach (var item in strToFile)
+            {
+                if (item.StartsWith("Text:"))
+                {
+                    TextFile f = new TextFile();
+                    f.Pars(item);
+                    files.Add(f);
+                }
+                if (item.StartsWith("Image:"))
+                {
+                    ImageFile f = new ImageFile();
+                    f.Pars(item);
+                    files.Add(f);
+                }
+
+                if (item.StartsWith("Movie:"))
+                {
+                    MovieFile f = new MovieFile();
+                    f.Pars(item);
+                    files.Add(f);
+                }
+            }
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
         }
     }
 }
